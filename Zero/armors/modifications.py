e8bb596d1e1346c47963659bbafc6c76c3d435b9
ZeroBossSq <82669735+ZeroBossSq@users.noreply.github.com>
2021-04-20 12:32:06 +0300

    Delete Zero directory

--- Zero/armors/modifications.py
+++
@@ -1,5 +0,0 @@
-class ModificationOfArmor:
-
-    def __init__(self, name, protect_points):
-        self.name = name
-        self.protect_points = protect_points
--- Zero/weapons/type_of_weapon.py
+++
@@ -1,6 +0,0 @@
-class TypeOfWeapon:
-
-    def __init__(self, name, damage, hint):
-        self.name = name
-        self.damage = damage
-        self.hint = hint
